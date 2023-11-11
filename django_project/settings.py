f0536840d4ffc1081eca98fe6e249bdf594c07ea
neriosoft <alabaneri@gmail.com>
2023-11-11 20:52:36 +0100

    add sqlite3

--- django_project/settings.py
+++ django_project/settings.py
@@ -84,12 +84,12 @@ WSGI_APPLICATION = "django_project.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 
-# DATABASES = {
-#    "default": {
-#        "ENGINE": "django.db.backends.sqlite3",
-#        "NAME": BASE_DIR / "db.sqlite3",
-#    }
-# }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
 
 
 # Password validation
