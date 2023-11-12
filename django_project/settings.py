c67231ca9277d26a3576a2eb369dde0974cbebd9
neriosoft <alabaneri@gmail.com>
2023-11-12 17:11:05 +0100

    use sqlite3 for render

--- django_project/settings.py
+++ django_project/settings.py
@@ -84,23 +84,23 @@ WSGI_APPLICATION = "django_project.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
-
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "church_db",
-        "USER": "neriosoft",
-        "PASSWORD": "Newcreation1.",
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql_psycopg2",
+#         "NAME": "church_db",
+#         "USER": "neriosoft",
+#         "PASSWORD": "Newcreation1.",
+#         "HOST": "127.0.0.1",
+#         "PORT": "5432",
+#     }
+# }
 
 
 # Password validation
