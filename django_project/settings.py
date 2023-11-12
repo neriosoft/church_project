3ad60b062704c8f12854037378d8c02e2972c03d
neriosoft <alabaneri@gmail.com>
2023-11-12 16:23:59 +0100

    downgrade django to 3.2.1 for render deployment

--- django_project/settings.py
+++ django_project/settings.py
@@ -58,13 +58,6 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-STORAGES = {
-    # ...
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
-
 
 ROOT_URLCONF = "django_project.urls"
 
@@ -154,6 +147,8 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # Media files (uploaded user files)
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
