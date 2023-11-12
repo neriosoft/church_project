18740ad9dff0db3c1e7f0bdd2dfd9b56667c99e7
neriosoft <alabaneri@gmail.com>
2023-11-12 19:13:24 +0100

    add whitenoise updated

--- django_project/settings.py
+++ django_project/settings.py
@@ -153,7 +153,7 @@ STATICFILES_DIRS = [BASE_DIR / "static"]  # for development
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
